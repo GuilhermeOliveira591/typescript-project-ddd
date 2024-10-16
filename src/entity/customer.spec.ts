67fdfb3e1e5cef584caf855cac8b5f6fb26f2144
Guilherme <guilhermegfoliveira2020@gmail.com>
2024-10-16 11:11:27 -0300

    feat: Starting to Implementing tests

---
+++ src/entity/customer.spec.ts
@@ -0,0 +1,52 @@
+import Address from "./address";
+import Customer from "./customer";
+
+describe("Customer unit tests", () => {
+    
+    it("Should throw error when id is empty", () =>{
+        expect(() => {
+            let customer = new Customer("", "John");
+        }).toThrow("Id is required");
+        
+    });
+
+    it("Should throw error when name is empty", () =>{
+        expect(() => {
+            let customer = new Customer("123", "");
+        }).toThrow("Name is required");
+        
+    });
+
+    it("Should change name", () =>{
+
+        const customer = new Customer("123", "John");
+
+        customer.changeName("Jane");
+
+        expect(customer.name).toBe("Jane");
+        
+    });
+
+    it("Should activate customer", () =>{
+
+        const customer = new Customer("1", "Customer1");
+        const address  = new Address("Street 1", 123, "01234-567", "São Paulo");
+        customer.changeAddress(address);
+
+        customer.activate();
+
+        expect(customer.isActive()).toBe(true);
+        
+    });
+
+    it("Should deactivate customer", () =>{
+
+        const customer = new Customer("1", "Customer1");
+
+        customer.deactivate();
+
+        expect(customer.isActive()).toBe(false);
+        
+    });
+
+})
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,11 +2,11 @@ import Address from "./address";
 
 export default  class Customer{
 
-    _id: string;
-    _name: string = "";
-    _address!: Address;
+    private _id: string;
+    private _name: string = "";
+    private _address!: Address;
     // nomeVariavel! -> quer dizer que vai inicializar em branco
-    _active: boolean = true;
+    private _active: boolean = true;
 
     constructor(id:string, name:string) {
         this._id = id;
@@ -14,19 +14,28 @@ export default  class Customer{
         this.validate();
     }
 
+    get name(): string {
+        return this._name;
+    }
+
     validate() {
-        if(this._name.length === 0){
-            throw new Error("Name is required!");
-        }
 
         if(this._id.length === 0){
             throw new Error("Id is required!");
+        }
+
+        if(this._name.length === 0){
+            throw new Error("Name is required!");
         }
     }
 
     changeName(name: string) {
         this._name = name;
         this.validate();
+    }
+
+    changeAddress(address: Address) {
+        this._address = address;
     }
 
     activate() {
@@ -39,6 +48,10 @@ export default  class Customer{
 
     deactivate() {
         this._active = false;
+    }
+
+    isActive(): boolean {
+        return this._active;
     }
 
 
