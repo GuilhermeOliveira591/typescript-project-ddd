dda89364a13f6e1e7045493b040a4d7ca2598aa1
Guilherme <guilhermegfoliveira2020@gmail.com>
2024-11-05 11:17:47 -0300

    feat: Order Repository Implemented

--- src/domain/entity/order_item.ts
+++ src/domain/entity/order_item.ts
@@ -1,17 +1,25 @@
 export default class OrderItem{
 
     private _id: string;
-    private product_id: string;
+    private _productId: string;
     private _name: string;
     private _price: number;
     private _quantity: number;
 
     constructor(id: string, name: string, price: number, productId: string, quantity: number){
         this._id = id;
-        this.product_id = productId;
+        this._productId = productId;
         this._name = name;
         this._price = price;
         this._quantity = quantity;
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    get name(): string {
+        return this._name;
     }
 
     get price(): number {
@@ -20,6 +28,10 @@ export default class OrderItem{
 
     get quantity(): number {
         return this._quantity;
+    }
+
+    get productId(): string {
+        return this._productId;
     }
 
     orderItemTotal(): number {
---
+++ src/infrastructure/db/sequelize/model/order-item.model.ts
@@ -0,0 +1,45 @@
+import { AllowNull, BelongsTo, Column, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import CustomerModel from "./customer.model";
+import ProductModel from "./product.model";
+import OrderModel from "./order.model";
+
+@Table({
+    tableName: "order_items",
+    timestamps: false,
+})
+
+export default class OrderItemModel extends Model{
+
+    @PrimaryKey
+    @Column
+    declare id: string;
+
+    @Column({allowNull: false})
+    declare name: string;
+
+    @Column({allowNull: false})
+    declare quantity: number;
+
+    @Column({allowNull: false})
+    declare price: number;
+
+    // ---- FOREIGN KEY
+    @ForeignKey(() => ProductModel)
+    @Column({allowNull: false})
+    declare product_id: string;
+
+    @ForeignKey(() => OrderModel)
+    @Column({allowNull: false})
+    declare order_id: string;
+
+    // ---- BELONGS TO
+    
+    @BelongsTo(() => OrderModel)
+    declare order: ProductModel;
+
+    @BelongsTo(() => ProductModel)
+    declare product: ProductModel;
+
+
+
+}
