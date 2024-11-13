20c8422ef89ecf617d39f68e7e3317a414caff44
Guilherme <guilhermegfoliveira2020@gmail.com>
2024-11-13 16:32:51 -0300

    Feat: Product domain events implemented

---
+++ src/domain/event/@shared/event-handler.interface.ts
@@ -0,0 +1,9 @@
+import EventInterface from "./event.interface";
+
+export default interface EventHandlerInterface<T extends EventInterface=EventInterface>{
+
+    
+    handle(event: T): void;
+
+
+}
